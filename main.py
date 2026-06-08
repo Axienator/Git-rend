45e395e03b4d675c0984e29caf2d4cece7e3e388
Phoolium <zupsaidoo123@gmail.com>
2026-06-09 00:09:01 +0800

    feat: add if statement to validate --duration flag

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import sys
 main_url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
 def git_trend():
+
     duration = {
         "day": "&since=day",
         "week": "&since=weekly",
         "month": "&since=monthly",
         "year": "&since=yearly"
     }
-    url = (f"{main_url}")
-    response = requests.get(url)
-
 
     user_choice = sys.argv[2]
+
+    if sys.argv[1] != "--duration":
+        print("Error: Invalid function, please use --duration followed by 'day', 'week', 'month', or 'year'")
+        return
+
     match user_choice:
-        case "day":
-            print("Day")
-        case "week":
-            print("Week")
-        case "month":
-            print("Month")
-        case "year":
-            print("Year")
- 
+        case "day" | "week" | "month" | "year":
+            time_filter = duration[user_choice]
+            print(f"Fetching data for the last {user_choice}...")
+        case _:
+            print("Error: Invalid time duration. Please choose 'day', 'week', 'month', or 'year'.")
+            return
+
+    url = f"{main_url}{time_filter}"
+    response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
