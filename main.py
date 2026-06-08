2e4968949b6e340f208c402bc9fd939775b12de0
Phoolium <zupsaidoo123@gmail.com>
2026-06-08 22:45:52 +0800

    refactor: fix argument bugs and implement dictionary with match case

--- main.py
+++ main.py
@@ -4,14 +4,33 @@ import sys
 main_url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
 def git_trend():
-    url = (f"{main_url}&page={sys.argv[1]}")
+    duration = {
+        "day": "&since=day",
+        "week": "&since=weekly",
+        "month": "&since=monthly",
+        "year": "&since=yearly"
+    }
+    url = (f"{main_url}")
+    response = requests.get(url)
+
+
+    user_choice = sys.argv[2]
+    match user_choice:
+        case "day":
+            print("Day")
+        case "week":
+            print("Week")
+        case "month":
+            print("Month")
+        case "year":
+            print("Year")
+ 
     
     if response.status_code == 200:
-        response = requests.get(url)
         data = response.json()
         return data
     else:
-        print("Error: Request failed with status code", response.status_code)
-        sys.exit(1)
+        print("Error: Request failed with status code:", response.status_code)
+        
 
-    
+git_trend()    
