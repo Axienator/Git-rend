4a3015c13686a1935a9fd8ed53db5389bec44bf7
Phoolium <zupsaidoo123@gmail.com>
2026-06-09 11:14:27 +0800

    fix: scoping bug and unexpected crashes prevented

--- main.py
+++ main.py
@@ -7,39 +7,45 @@ main_url = "https://api.github.com/search/repositories"
 
 def git_trend():
 
-    duration = {
-        "day": "&since=day",
-        "week": "&since=weekly",
-        "month": "&since=monthly",
-        "year": "&since=yearly"
-    }
-
-    if len(sys.argv) != 6 or sys.argv[1] != "trending-repos" or sys.argv[2] != "--duration" or sys.argv[4] != "--limit":
-        print("Error: Invalid function call. Please use --duration followed by 'day', 'week', 'month', or 'year' and --limit followed by a number.")
+    if( 
+    len(sys.argv) != 6
+    or sys.argv[1] != "trending-repos"
+    or sys.argv[2] != "--duration"
+    or sys.argv[4] != "--limit"
+    ):
+        print("Error: Invalid arguments format")
         return
+    
+    duration = sys.argv[3]
+    repo_limit = int(sys.argv[5])
 
-    user_choice = sys.argv[3]
-    repo_limit = sys.argv[5]
-
-    match user_choice:
-        case "day" | "week" | "month" | "year":
-            time_filter = duration[user_choice]
-            print(f"Fetching data for the last {user_choice}...\n")
+    match duration:
+        case "day":
+            date_str = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+        case "week":
+            date_str = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+        case "month":
+            date_str = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d")
+        case "year":
+            date_str = (datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d")
         case _:
-            print("Error: Invalid time duration. Please choose 'day', 'week', 'month', or 'year'.")
+            print("Error: Invalid duration value")
             return
 
-    url = f"{main_url}{time_filter}&per_page={repo_limit}"
+
+    url = f"{main_url}?q=language:python+created:>{date_str}&sort=stars&order=desc&per_page={repo_limit}"
     response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
     else:
         print("Error: Request failed with status code:", response.status_code)
+        return
 
     if data:
         for repo in data['items']:
             print(f"Repository Name: {repo['name']}")
+            print(f"Username: {repo['owner']['login']}")
             print(f"Stars: {repo['stargazers_count']}")
             print(f"URL: {repo['html_url']}")
             print("-" * 40)
