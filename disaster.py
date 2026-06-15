8528eaa5c944d0181ad131c503e2d8c5658979b8
Phoolium <zupsaidoo123@gmail.com>
2026-06-15 10:20:50 +0800

    feat add error handling and safe parsing for API response

--- disaster.py
+++ disaster.py
@@ -1,20 +1,19 @@
 import requests
-import sys
+from argparse import ArgumentParser
 
-main_url = 'https://api.terraquakeapi.com/v1/earthquakes/today'
+main_url = 'https://api.terraquakeapi.com/v1/earthquakes'
 
-def earthquake():
-  response = requests.get(main_url, params={'limit': 10})
-  data = response.json()
-  print(f"Total earthquakes today: {data['totalEarthquakes']}")
+def earthquake() -> None:
+  parser = ArgumentParser(description='fetches information about the TerraQuake API')
+  parser.add_argument('--limit')
+  args = parser.parse_args()
 
-  quake_limit = int(sys.argv[2])
+  response = requests.get(main_url, params={'limit':args})
 
-  for quake in data['payload']:
-      props = quake['properties']
-
-      print(f"Time: {props['time']}")
-      print(f"Magnitue: {props['mag']}")
-      print(f"Location: {props['place']}")
+  if response.status_code == 200:
+    data = response.json()
+    print(f"Total earthquakes today: {data['totalEarthquakes']}")
+  else:
+     print(f"Error:{response.status_code}")
 
 earthquake()
